d513fc05e18df1ac817ff5c1974905aabaae6457
belal <bamer8353@gmail.com>
2024-08-15 22:35:19 +0200

    Get Hotel Room(s)

--- app/Models/HotelRooms.php
+++ app/Models/HotelRooms.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use App\Http\filters\QueryFilter;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class HotelRooms extends Model
+class HotelRoom extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function scopeFilter(Builder $builder, QueryFilter $filter)
+    {
+        return $filter->apply($builder);
+    }
+    public function hotel() {
+        return $this->belongsTo(Hotel::class);
+    }
 }
