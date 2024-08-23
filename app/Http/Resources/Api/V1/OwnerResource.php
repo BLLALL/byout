836add1eefc9700b4758e77821cd04c133de1116
Belal Amer <bamer8353@gmail.com>
2024-08-23 16:29:53 +0200

    Post bus and schedule tours

---
+++ app/Http/Resources/Api/V1/OwnerResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OwnerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->user_id,
+            "name" => $this->user->name,
+            "email" => $this->user->email,
+            "phone_number" => $this->user->phone_number,
+            "age" => $this->user->age,
+            "marital_status" => $this->user->marital_status,
+            "current_job" => $this->user->current_job,
+            "profile_image" => $this->user->profile_image,
+            "role" => $this->role,
+            "organization" => $this->organization,
+            "identification_card" => $this->identification_card,
+            "licensing" => $this->licensing,
+            "affiliation_certificate" => $this->affiliation_certificate,
+            "commercial_register" => $this->commercial_register,
+        ];
+    }
+}
